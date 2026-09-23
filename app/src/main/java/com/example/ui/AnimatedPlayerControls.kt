6a3406e5348f0fcf0f4f3665dd0c07d7973560fc
happyiyate-hash <happyiyate@gmail.com>
2026-09-23 12:49:38 +0100

    Clean repeat control imports

--- app/src/main/java/com/example/ui/AnimatedPlayerControls.kt
+++ app/src/main/java/com/example/ui/AnimatedPlayerControls.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.graphics.PathMeasure
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlin.math.PI
 import kotlin.math.atan2
